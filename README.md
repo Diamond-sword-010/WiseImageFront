22cbdef9ce55695be5d090498999a390abd7269b
NivShirazi <nivshirazi9@gmail.com>
2023-03-12 14:08:26 -0700

    remove all

--- README.md
+++
@@ -1,3 +0,0 @@
-# flutter_map_test
-
-A new Flutter project.
--- lib/main.dart
+++
@@ -1,9 +0,0 @@
-// ignore_for_file: prefer_const_constructors
-
-import 'package:flutter/material.dart';
-
-import 'my_app.dart';
-
-void main() {
-  runApp(MainApp());
-}
